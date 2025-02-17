04921588b59f996ba04ed514e2e64fe64f29ae0e
PinguinLars1105 <Heijmen.Lars@gmail.com>
2025-02-17 15:14:24 +0100

    Fatal Error in dependency

--- src/main/java/nl/pinguinlars/pinguinterm/Main.java
+++ src/main/java/nl/pinguinlars/pinguinterm/Main.java
@@ -24,28 +24,19 @@
 
 package nl.pinguinlars.pinguinterm;
 
-import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-public class AppLauncher extends Application {
+public class Main {
     static SerialController serial = new SerialController();
     static LogHandler Logger = new LogHandler(false);
     static LogHandler ErrorLogger = new LogHandler(true);
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        App.Launch();
         Logger.Log("Starting Application");
         if (serial.MicroBitPort == null) {
             ErrorMessage.Launch();
             ErrorLogger.Log("No MicroBit Found");
             Logger.Log("Exiting Application");
-            throw new IOException("No MicroBit found");
+            throw new RuntimeException("No MicroBit found");
         }
         serial.ReadProcess.submit(() -> {
             byte[] buffer = new byte[1024];
@@ -64,70 +55,12 @@ public class AppLauncher extends Application {
         });
         try {
             serial.MicroBitPort.openPort();
-            launch(args);
+            App.Launch();
         } catch (Exception e) {
             ErrorLogger.Log(e.getMessage());
             serial.ReadProcess.shutdownNow();
             serial.MicroBitPort.closePort();
         }
         serial.Shutdown();
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        Scene scene = new Scene(createContents(), 960, 540);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    private Region createContents() {
-        VBox Return = new VBox(20, Button0_0(), Button1_0(), Button2_0(), Button3_0(), Button4_0());
-        Return.setAlignment(Pos.CENTER_LEFT);
-        return Return;
-    }
-
-    private Button Button0_0() {
-        Button Return = new Button("Forward");
-        Return.setOnAction(evt -> System.out.println("Hello"));
-        return Return;
-    }
-
-    private Button Button1_0() {
-        Button Return = new Button("Forward");
-        Return.setOnAction(evt -> System.out.println("Hello"));
-        return Return;
-    }
-
-    private Button Button2_0() {
-        Button Return = new Button("Forward");
-        Return.setOnAction(evt -> System.out.println("Hello"));
-        return Return;
-    }
-
-    private Button Button3_0() {
-        Button Return = new Button("Forward");
-        Return.setOnAction(evt -> {
-            System.out.println("Hello");
-//            SerialControllerDeprecated.SendMessage("Forward"); //Not tested
-        });
-        return Return;
-    }
-
-    private Button Button4_0() {
-        Button Return = new Button("Forward");
-        Return.setOnAction(evt -> System.out.println("Hello"));
-        return Return;
-    }
-
-    private Button Button0_1() {
-        Button Return = new Button("Forward");
-        Return.setOnAction(evt -> System.out.println("Hello"));
-        return Return;
-    }
-
-    private Button Button1_1() {
-        Button Return = new Button("Forward");
-        Return.setOnAction(evt -> System.out.println("Hello"));
-        return Return;
     }
 }
