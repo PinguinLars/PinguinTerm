4690a5888734141543099359d2531bc0902eb2e6
Lars <heijmen.lars@gmail.com>
2025-03-02 11:52:37 +0100

    Small change [ci skip]

--- src/main/java/nl/pinguinlars/pinguinterm/App.java
+++ src/main/java/nl/pinguinlars/pinguinterm/App.java
@@ -33,6 +33,8 @@ import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    private static final int ButtonVSize = 140, ButtonV1Size = 110;
+
     public static void Launch() {
         launch();
     }
@@ -85,7 +87,7 @@ public class App extends Application {
 
     private Button Button0_0() {
         Button button0_0 = new Button("0.0");
-        button0_0.setPrefSize(131,108);
+        button0_0.setPrefSize(ButtonVSize, ButtonV1Size);
         button0_0.setId("0_0");
         button0_0.setOnAction(e -> {
             //Hi :wave:
@@ -95,6 +97,7 @@ public class App extends Application {
 
     private Button Button0_1() {
         Button button0_1 = new Button("0.1");
+        button0_1.setPrefSize(ButtonVSize, ButtonV1Size);
         button0_1.setId("0_1");
         button0_1.setOnAction(e -> {
             //Hi :wave:
@@ -104,6 +107,7 @@ public class App extends Application {
 
     private Button Button0_2() {
         Button button0_2 = new Button("0.2");
+        button0_2.setPrefSize(ButtonVSize, ButtonV1Size);
         button0_2.setId("0_2");
         button0_2.setOnAction(e -> {
             //Hi :wave:
@@ -113,6 +117,7 @@ public class App extends Application {
 
     private Button Button0_3() {
         Button button0_3 = new Button("0.3");
+        button0_3.setPrefSize(ButtonVSize, ButtonV1Size);
         button0_3.setId("0_3");
         button0_3.setOnAction(e -> {
             //Hi :wave:
@@ -122,7 +127,7 @@ public class App extends Application {
 
     private Button Button0_4() {
         Button button0_4 = new Button("0.4");
-        button0_4.setId("0_4");
+        button0_4.setPrefSize(ButtonVSize, ButtonV1Size);
         button0_4.setId("0_4");
         button0_4.setOnAction(e -> {
             //Hi :wave:
@@ -132,6 +137,7 @@ public class App extends Application {
 
     private Button Button1_0() {
         Button button1_0 = new Button("1.0");
+        button1_0.setPrefSize(ButtonVSize, ButtonV1Size);
         button1_0.setId("1_0");
         button1_0.setOnAction(e -> {
             //Hi :wave:
@@ -141,7 +147,7 @@ public class App extends Application {
 
     private Button Button1_1() {
         Button button1_1 = new Button("\u21D6");
-        button1_1.setPrefSize(131,108);
+        button1_1.setPrefSize(ButtonVSize, ButtonV1Size);
         button1_1.setId("1_1");
         button1_1.setOnAction(e -> {
             //Hi :wave:
@@ -151,8 +157,8 @@ public class App extends Application {
 
     private Button Button1_2() {
         Button button1_2 = new Button("\u21D0");
+        button1_2.setPrefSize(ButtonVSize, ButtonV1Size);
         button1_2.setId("1_2");
-        button1_2.setPrefSize(131,108);
         button1_2.setOnAction(e -> {
             //Hi :wave:
         });
@@ -161,6 +167,7 @@ public class App extends Application {
 
     private Button Button1_3() {
         Button button1_3 = new Button("1.3");
+        button1_3.setPrefSize(ButtonVSize, ButtonV1Size);
         button1_3.setId("1_3");
         button1_3.setOnAction(e -> {
             //Hi :wave:
@@ -170,6 +177,7 @@ public class App extends Application {
 
     private Button Button1_4() {
         Button button1_4 = new Button("1.4");
+        button1_4.setPrefSize(ButtonVSize, ButtonV1Size);
         button1_4.setId("1_4");
         button1_4.setOnAction(e -> {
             //Hi :wave:
@@ -179,6 +187,7 @@ public class App extends Application {
 
     private Button Button2_0() {
         Button button2_0 = new Button("2.0");
+        button2_0.setPrefSize(ButtonVSize, ButtonV1Size);
         button2_0.setId("2_0");
         button2_0.setOnAction(e -> {
             //Hi :wave:
@@ -188,7 +197,7 @@ public class App extends Application {
 
     private Button Button2_1() {
         Button button2_1 = new Button("\u21D1");
-        button2_1.setPrefSize(131,108);
+        button2_1.setPrefSize(ButtonVSize, ButtonV1Size);
         button2_1.setId("2_1");
         button2_1.setOnAction(e -> {
             //Hi :wave:
@@ -198,7 +207,7 @@ public class App extends Application {
 
     private Button Button2_2() {
         Button button2_2 = new Button("\u20DD");
-        button2_2.setPrefSize(131,108);
+        button2_2.setPrefSize(ButtonVSize, ButtonV1Size);
         button2_2.setId("2_2");
         button2_2.setOnAction(e -> {
             //Hi :wave:
@@ -208,7 +217,7 @@ public class App extends Application {
 
     private Button Button2_3() {
         Button button2_3 = new Button("\u21D3");
-        button2_3.setPrefSize(131,108);
+        button2_3.setPrefSize(ButtonVSize, ButtonV1Size);
         button2_3.setId("2_3");
         button2_3.setOnAction(e -> {
             //Hi :wave:
@@ -218,6 +227,7 @@ public class App extends Application {
 
     private Button Button2_4() {
         Button button2_4 = new Button("2.4");
+        button2_4.setPrefSize(ButtonVSize, ButtonV1Size);
         button2_4.setId("2_4");
         button2_4.setOnAction(e -> {
             //Hi :wave:
@@ -227,6 +237,7 @@ public class App extends Application {
 
     private Button Button3_0() {
         Button button3_0 = new Button("3.0");
+        button3_0.setPrefSize(ButtonVSize, ButtonV1Size);
         button3_0.setId("3_0");
         button3_0.setOnAction(e -> {
             //Hi :wave:
@@ -236,7 +247,7 @@ public class App extends Application {
 
     private Button Button3_1() {
         Button button3_1 = new Button("\u21D7");
-        button3_1.setPrefSize(131,108);
+        button3_1.setPrefSize(ButtonVSize, ButtonV1Size);
         button3_1.setId("3_1");
         button3_1.setOnAction(e -> {
             //Hi :wave:
@@ -246,7 +257,7 @@ public class App extends Application {
 
     private Button Button3_2() {
         Button button3_2 = new Button("\u21D2");
-        button3_2.setPrefSize(131,108);
+        button3_2.setPrefSize(ButtonVSize, ButtonV1Size);
         button3_2.setId("3_2");
         button3_2.setOnAction(e -> {
             //Hi :wave:
@@ -256,6 +267,7 @@ public class App extends Application {
 
     private Button Button3_3() {
         Button button3_3 = new Button("3.3");
+        button3_3.setPrefSize(ButtonVSize, ButtonV1Size);
         button3_3.setId("3_3");
         button3_3.setOnAction(e -> {
             //Hi :wave:
@@ -265,6 +277,7 @@ public class App extends Application {
 
     private Button Button3_4() {
         Button button3_4 = new Button("3.4");
+        button3_4.setPrefSize(ButtonVSize, ButtonV1Size);
         button3_4.setId("3_4");
         button3_4.setOnAction(e -> {
             //Hi :wave:
@@ -273,31 +286,38 @@ public class App extends Application {
     }
 
     private Button Button4_0() {
-        Button Return = new Button("4.0");
-        Return.setOnAction(e -> {
+        Button button4_0 = new Button("4.0");
+        button4_0.setPrefSize(ButtonVSize, ButtonV1Size);
+        button4_0.setId("4_0");
+        button4_0.setOnAction(e -> {
             //Hi :wave:
         });
-        return Return;
+        return button4_0;
     }
 
     private Button Button4_1() {
-        Button Return = new Button("4.1");
-        Return.setOnAction(e -> {
+        Button button4_1 = new Button("4.1");
+        button4_1.setPrefSize(ButtonVSize, ButtonV1Size);
+        button4_1.setId("4_1");
+        button4_1.setOnAction(e -> {
             //Hi :wave:
         });
-        return Return;
+        return button4_1;
     }
 
     private Button Button4_2() {
-        Button Return = new Button("4.2");
-        Return.setOnAction(e -> {
+        Button button4_2 = new Button("4.2");
+        button4_2.setPrefSize(ButtonVSize,ButtonV1Size);
+        button4_2.setId("4_2");
+        button4_2.setOnAction(e -> {
             //Hi :wave:
         });
-        return Return;
+        return button4_2;
     }
 
     private Button Button4_3() {
         Button button4_3 = new Button("4.3");
+        button4_3.setPrefSize(ButtonVSize,ButtonV1Size);
         button4_3.setId("4_3");
         button4_3.setOnAction(e -> {
             //Hi :wave:
@@ -307,6 +327,7 @@ public class App extends Application {
 
     private Button Button4_4() {
         Button button4_4 = new Button("4.4");
+        button4_4.setPrefSize(ButtonVSize,ButtonV1Size);
         button4_4.setId("4_4");
         button4_4.setOnAction(e -> {
             //Hi :wave:
